5043b848b4abeb990ca10d70c20e2ba883151d8c
okccc <1573976179@qq.com>
2021-05-19 14:34:46 +0800

    init

---
+++ src/test/java/com/okccc/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okccc.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
